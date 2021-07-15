6b7c140af1260f81807d62b20aa6e2b193472ec9
TrNz4869 <pay18wos@gmail.com>
2021-07-15 13:06:26 +0700

    4. Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,26 @@
-import React, {useState} from 'react';
-import {ImageBackground, Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ImageBackground, Text, View, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=ba71be1f648c0f1670bfeba150347064`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+    
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
         description: 'description',
@@ -26,15 +44,15 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%'
     },
-    
-    box:{
-        flexDirection:'column',
-        alignItems:'center',
+
+    box: {
+        flexDirection: 'column',
+        alignItems: 'center',
         backgroundColor: 'rgba(52, 52, 52, 0.5)',
         width: '100%',
     },
 
-    zip_code_text:{
+    zip_code_text: {
         padding: 20,
     },
 
